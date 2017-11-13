0c9ba6871039c0b6a45a71b6eb9bc96fa7292dad
Md. Samshad Rahman <samshad.ru@gmail.com>
2017-11-13 21:50:13 +0600

    NOV 2017

---
+++ 12376 - As Long as I Learn, I Live.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define si(x) scanf("%d",&x)
+#define sii(x,y) scanf("%d %d",&x,&y)
+#define siii(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sl(x) scanf("%lld",&x)
+#define sll(x,y) scanf("%lld %lld",&x,&y)
+#define slll(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define ss(ch) scanf("%s",ch)
+#define pi(x) printf("%d",x)
+#define pii(x,y) printf("%d %d",x,y)
+#define pl(x) printf("%lld",x)
+#define pll(x,y) printf("%lld %lld",x,y)
+#define sf scanf
+#define pf printf
+#define pcs(x,y) printf("Case %d: %s", x, y)
+#define pci(x,y) printf("Case %d: %d", x, y)
+#define pcl(x,y) printf("Case %d: %lld", x, y)
+#define NL printf("\n")
+#define nl '\n'
+#define mod 1000000007
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+
+#define FOR(i,j,k) for(int i = j; i < k; i++)
+#define rep(l,n) FOR(l,0,n)
+#define per(i,j,k) for(int i = j; i > k; i--)
+
+#define PI acos(-1.0)
+#define eps 1e-9
+
+#define pb(x) push_back(x)
+#define ppb() pop_back()
+#define sz(x) x.size()
+#define xx first
+#define yy second
+#define mp(a,b) make_pair(a,b)
+#define ssz(a) strlen(a)
+
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define clr(ara) mem(ara,0)
+#define st(ara) mem(ara,-1)
+#define all(a) a.begin(),a.end()
+//cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+
+#define debug(args...) {dbg,args; cerr<<endl;}
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cout<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef pair<int,int> PII;
+typedef vector< PII > VII;
+
+int dx[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+///=>=>=>=>=>=>=>=>=>00100<=<=<=<=<=<=<=<=<=///
+
+VI v[111];
+int val[111], last, ans, n, m;
+
+void khali(){
+    rep(l,111) v[l].clear();
+    last = ans = 0;
+}
+
+void dfs(int u){
+    last = u;
+
+    int mx = 0;
+
+    for(auto i : v[u]) mx = max(mx,val[i]);
+
+    for(auto i : v[u])
+        if(val[i] == mx)
+            dfs(i);
+
+    ans += mx;
+}
+
+int main(){
+    //std::ios_base::sync_with_stdio(0);cin.tie(0);
+    //FI;//FO;
+
+    int t = 0, z = 0, len;
+
+    //int n = 0, k = 0, m = 0; int ans = 0;
+
+    si(t);
+
+    while(t--){
+        khali();
+
+        sii(n,m);
+        rep(l,n) si(val[l]);
+        rep(l,m){
+            int x, y;
+            sii(x,y);
+            v[x].pb(y);
+        }
+
+        dfs(0);
+
+        pf("Case %d: %d %d\n", ++z, ans, last);
+    }
+
+    return 0;
+}
